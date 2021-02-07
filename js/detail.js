3a7b38446d688d828373c3608c257e5a23e8d8cc
ben <noahbenaas@gmail.com>
2021-02-07 16:54:28 +0100

    update detail page

--- js/detail.js
+++ js/detail.js
@@ -2,86 +2,80 @@ import { ROCKETS_URL } from "./index.js";
 
 const queryString = document.location.search;
 
-
 const params = new URLSearchParams(queryString);
 
-
 const id = params.get("id");
-
-
-
 
 if (!id) {
     document.location.href = "/";
 }
 
-
 const detailsUrl = ROCKETS_URL + "/" + id;
-
 
 (async function () {
     try {
         const response = await fetch(detailsUrl);
+
         const details = await response.json();
-
-
 
         document.title = details.name;
 
-        console.log("details", details.flickr_images);
+        console.log("details", details.flickr_images[0]);
 
         const container = document.querySelector(".detail-container");
 
-
         container.innerHTML = `
-     
-        <div class="container">
+        <div class="container detail">
         <div class="row">
-            <div class="col-sm">
-            <div id="carouselExampleIndicators" class="carousel slide" data-interval="false" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner embed-responsive embed-responsive-21by9">
-              <div class="carousel-item active embed-responsive-item" style="background-image: url(img/slider-1.jpg);"></div>
-              <div class="carousel-item embed-responsive-item" style="background-image: url(img/slider-2.jpg);"></div>
-              <div class="carousel-item embed-responsive-item" style="background-image: url(img/slider-3.jpg);"></div>
-            </div>
-          </div>
-
-
-
-
-
-            </div>
-            <div class="col-sm">
-                <div class="project-info-box mt-0">
-                    <h5>${details.name}</h5>
-                    <p class="mb-0">${details.description}</p>
-                </div>
-                <div class="project-info-box">
-                    <p><b>Type:</b>${details.type}</p>
-                    <p><b>first flight:</b>${details.first_flight}</p>
-                    <p><b>Country:</b> ${details.country}</p>
-                    <p><b>Material:</b> ${details.landing_legs.material}</p>
-                    <p class="mb-0"><b>Budget:</b>${details.cost_per_launch} $500</p>
-                </div>
-            </div>
-        </div>
+        <div class="col-md-6">
+        <div class="col-md-12">
+          <div class="project-info-box mt-0">
+          <h5>${details.name}</h5>
+          <p class="mb-0">${details.description}</p>
+      </div>
+        <div class="project-info-box">
+         <p><b>Type:</b> ${details.type}</p>
+        <p><b>First flight:</b> ${details.first_flight}</p>
+         <p><b>Country:</b> ${details.country}</p>
+        <p><b>Material:</b> ${details.landing_legs.material}</p>
+        <p class="mb-0"><b>Cost per Launch:</b> $${details.cost_per_launch} </p>
+     </div>
+     </div>
     </div>
-
-                            
-                            
-
-
-                            `;
-
-
+    <div class="col-md-6">
+    <div class="col-md-12">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="${details.flickr_images[1]}" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="${details.flickr_images[2]}" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="${details.flickr_images[3]}" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+ </a>
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+   <span class="carousel-control-next-icon" aria-hidden="true"></span>
+ </a>
+</div>  
+<div class ="image-row row "> 
+<img class="detailimage" src="${details.flickr_images[0]}" alt="">  
+<img class="detailimage" src="${details.flickr_images[1]}" alt="">
+<img class="detailimage" src="${details.flickr_images[2]}" alt="">
+<img class="detailimage" src="${details.flickr_images[3]}" alt="">
+            </div>           
+        </div> 
+    </div>        
+</div>
+    </div>
+        </div>    
+          </div>`;
     } catch (error) {
-        console.log(error);;
+        console.log(error);
     }
 })();
-
-
--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 import displayNextLaunch from "./displayNextLaunch.js";
 import updateEverySec from "./updateEverySec.js";
+import createRocketCards from "./createRocketCards.js";
 // picking up the correct url
 let NEXT_LAUNCH = `https://api.spacexdata.com/v4/launches/next`;
 
 export const ROCKETS_URL = `https://api.spacexdata.com/v4/rockets`;
 let rocketsArray = [];
 
-// console.log(ROCKETS_URL);
 
-// this variabel is going to be fill up later
+
 
 // asyn function that is going to fetch
 async function getProducts() {
@@ -25,7 +25,7 @@ async function getProducts() {
 }
 getProducts();
 
-// this variabel is going to be fill up later
+
 
 // asyn function that is going to fetch
 async function getRockets() {
@@ -43,27 +43,6 @@ async function getRockets() {
 getRockets();
 
 
-function createRocketCards(rockets) {
-    const cardsContainer = document.querySelector(".cards");
-    rockets.forEach(rocket => {
-
-        let img = rocket.flickr_images[0];
-        if (img.includes("imgur")) {
-            img = "img/spacex-6.png"
-        }
-        cardsContainer.innerHTML += `
-        <div class="col-12 col-md-3">
-        <div class=" card h-100 ">
-        <img class="card-img-top " src="${img}" alt="Card image cap">
-            <div class="card-body" >
-                <h3 class="card-title">${rocket.name}</h3>
-                <p class="card-text">${rocket.description}</p>
-                <a class="btn btn-primary" href="detail.html?id=${rocket.id}" > Details</a>
-                </div>
-        </div>
-    </div> `;
-    });
-}
 
 
 
