d496716096f1fe4a621b1dee26b2abc65b09e539
ben <noahbenaas@gmail.com>
2021-02-11 17:19:26 +0100

    update

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ export const ROCKETS_URL = `https://api.spacexdata.com/v4/rockets`;
 let rocketsArray = [];
 
 
+let ABOUT_URL = `https://api.spacexdata.com/v4/company`;
 
 
 // asyn function that is going to fetch
@@ -42,6 +43,69 @@ async function getRockets() {
     }
 }
 getRockets();
+
+
+// asyn function that is going to fetch
+async function getCompanyInformation() {
+    try {
+        const response = await fetch(ABOUT_URL);
+        const json = await response.json();
+        let data = json;
+        createCompanyList(data);
+    } catch (error) {
+        console.log(error);
+        // displayMessage("error", error, "container");
+    }
+}
+getCompanyInformation();
+
+function createCompanyList(information) {
+    const lisContainer = document.querySelector(".list");
+
+    console.log("information", information.headquarters);
+    lisContainer.innerHTML += `
+    <div id="accordionExample" class="accordion shadow">
+    <!-- Accordion item 1 -->
+    <div class="card">
+        <div id="headingOne" class="card-header bg-white shadow-sm border-0">
+            <h2 class="mb-0">
+                <button type="button" data-toggle="collapse" data-target="#collapseOne"
+                    aria-expanded="true" aria-controls="collapseOne"
+                    class="btn btn-link text-dark font-weight-bold text-uppercase collapsible-link">headquarters</button>
+            </h2>
+        </div>
+        <div id="collapseOne" aria-labelledby="headingOne" data-parent="#accordionExample"
+            class="collapse show">
+            <div class="card-body">
+            <ul class="list-group">
+            <li class="list-group-item">Address: ${information.headquarters.address}</li>
+            <li class="list-group-item">City: ${information.headquarters.city}</li>
+            <li class="list-group-item">State:${information.headquarters.state}</li>
+          </ul>
+            </div>
+        </div>
+    </div><!-- End -->
+</div><!-- End -->
+      
+    
+    `;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
